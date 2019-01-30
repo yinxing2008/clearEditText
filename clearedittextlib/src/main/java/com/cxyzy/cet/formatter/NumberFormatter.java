0ecd1179183f9678f79f1c34e23b6342a157ed43
jerry <309032663@qq.com>
2019-01-30 18:21:05 +0800

    update

--- clearedittextlib/src/main/java/com/cxyzy/cet/formatter/NumberFormatter.java
+++ clearedittextlib/src/main/java/com/cxyzy/cet/formatter/NumberFormatter.java
@@ -3,6 +3,9 @@ package com.cxyzy.cet.formatter;
 import android.text.InputFilter;
 import android.text.InputType;
 
+/**
+ * 数字输入格式化器，支持输入数字和小数点
+ */
 public class NumberFormatter extends BaseInputTextFormatter {
     @Override
     protected boolean isSpace(int length) {
