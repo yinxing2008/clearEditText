c1db55465e3cf99ea6f63ef5d280b1414380e687
jerry <309032663@qq.com>
2019-04-15 14:23:34 +0800

    update

--- app_using_lib/src/main/java/com/cxyzy/app_using_lib/MainActivity.kt
+++ app_using_lib/src/main/java/com/cxyzy/app_using_lib/MainActivity.kt
@@ -2,16 +2,11 @@ package com.cxyzy.app_using_lib
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var tag = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-            log(tag,phoneCET.textWithoutBlanks.toString())
-        }
     }
 }
